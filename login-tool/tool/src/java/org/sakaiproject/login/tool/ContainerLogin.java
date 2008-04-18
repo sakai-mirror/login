f1ca881ff3e5f42a671af09ca624ae5aeafc9b21
jlrenfro@ucdavis.edu <jlrenfro@ucdavis.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-04-18 00:01:10 +0000

    SAK-13297, SAK-12841
    Adding xlogin.vm template support modeled after Ian's SkinnableCharonPortal. This is an interim commit. Basic functionality is there, but
    needs some further refinements.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/login/branches/SAK-13297@45528 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- login-tool/tool/src/java/org/sakaiproject/login/tool/ContainerLogin.java
+++ login-tool/tool/src/java/org/sakaiproject/login/tool/ContainerLogin.java
@@ -128,7 +128,7 @@ public class ContainerLogin extends HttpServlet
 		}
 
 		// mark the session and redirect (for login failuer or authentication exception)
-		session.setAttribute(LoginTool.ATTR_CONTAINER_CHECKED, LoginTool.ATTR_CONTAINER_CHECKED);
-		res.sendRedirect(res.encodeRedirectURL((String) session.getAttribute(LoginTool.ATTR_RETURN_URL)));
+		session.setAttribute(SkinnableLogin.ATTR_CONTAINER_CHECKED, SkinnableLogin.ATTR_CONTAINER_CHECKED);
+		res.sendRedirect(res.encodeRedirectURL((String) session.getAttribute(SkinnableLogin.ATTR_RETURN_URL)));
 	}
 }
